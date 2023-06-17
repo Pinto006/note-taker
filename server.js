1b7e101e9d21d20c0cd4ce5f8670f23292aecbea
Wave FC <malloriepinto89@gmail.com>
2023-06-16 17:56:05 -0700

    adding routes to server.js

--- server.js
+++ server.js
@@ -1,4 +1,38 @@
 const express = require('express');
-const path = require('path');
+const htmlRoutes = require('./routes/html-routes');
+const apiRoutes = require('./routes/apiroutes');
 
 const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Import custom middleware, "cLog"
+app.use(clog);
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(htmlRoutes);
+app.use(apiRoutes);
+
+
+
+// GET Route for homepage
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for feedback page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
