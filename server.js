48d5335db0902dd0d95f16b27aa291af0451e10a
Wave FC <malloriepinto89@gmail.com>
2023-06-24 11:29:41 -0700

    added route files

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
+
 const path = require('path');
-const fs = require('fs');
-// const util = require('util');
+const api = require('./routes/api.js');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,53 +11,54 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(api);
+
 
 // HTML Routes
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('/', (req, res) =>
+app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 
 
-//API Routes 
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (error, data) => {
-        error ? res.json(error) : res.json(data)
-    })
- });
+// //API Routes 
+// app.get('/api/notes', (req, res) => {
+//     fs.readFile('./db/db.json', 'utf8', (error, data) => {
+//         error ? res.json(error) : res.json(data)
+//     })
+//  });
 
-app.post('/api/notes', function (req, res) {
-    const { title, text } = req.body;
-    fs.readFile('./db/db.json', (error, data) => {
-        if (req.body) {
-            const newNote = {
-                title,
-                text,
-                id: uuidv4()
-            };
-            fs.readAndAppend(newNote, './db/db.json');
-            fs.writeFile('./db/db.json', JSON.stringify(newNote));
-            res.json(newNote);
-        }
-    })
-})
+// app.post('/api/notes', function (req, res) {
+//     const { title, text } = req.body;
+//     fs.readFile('./db/db.json', (error, data) => {
+//         if (req.body) {
+//             const newNote = {
+//                 title,
+//                 text,
+//                 id: uuidv4()
+//             };
+//             fs.readAndAppend(newNote, './db/db.json');
+//             fs.writeFile('./db/db.json', JSON.stringify(newNote));
+//             res.json(newNote);
+//         }
+//     })
+// })
 
-app.delete('/api/notes/:id', (req, res) => {
-    let data = fs.readFileSync('db/db.json', 'utf8');
-    const jsonData = JSON.parse(data);
-    const newNote = jsonData.filter((note) => {
-        return note.id !== req.params.id;
-    });
-    fs.writeFileSync('db/db.json', JSON.stringify(newNote))
-    res.json("note deleted");
-});
+// app.delete('/api/notes/:id', (req, res) => {
+//     let data = fs.readFileSync('db/db.json', 'utf8');
+//     const jsonData = JSON.parse(data);
+//     const newNote = jsonData.filter((note) => {
+//         return note.id !== req.params.id;
+//     });
+//     fs.writeFileSync('db/db.json', JSON.stringify(newNote))
+//     res.json("note deleted");
+// });
 
 //Listening
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
-//read,uuid, push(append), write,send note,res.json
